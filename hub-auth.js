b7044eec0d717eedea30d01cc6e5beb3b16634ff
Bryan Jaworski <jaworskibc@gmail.com>
2026-07-27 23:48:56 -0400

    Move lead demos into hub site demos

--- hub-auth.js
+++ hub-auth.js
@@ -1,6 +1,5 @@
 (function () {
   if (sessionStorage.getItem('jaw_auth')) return;
   sessionStorage.setItem('jaw_return', location.href);
-  var loginPath = location.pathname.indexOf('/toolkit/') !== -1 ? '../login.html' : 'login.html';
-  location.replace(loginPath);
+  location.replace('/login.html');
 })();
